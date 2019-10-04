b799276e1b6067896636fb16dcf3e3cd18b72cfc
Tiago munerato <50157378+Tiagomunerato@users.noreply.github.com>
2019-10-04 19:03:56 -0300

    Add files via upload

---
+++ ex7/ex7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cf, comissao, impostos, custo_final;
+
+            Console.Write("Informe o custo de fábrica do veículo: ");
+            cf = double.Parse(Console.ReadLine());
+
+            comissao = cf * (25.0 / 100.0);
+            impostos = cf * (45.0 / 100.0);
+
+            custo_final = cf + comissao + impostos;
+            //custo_final = cf + (cf * (25.0/100.0)) + (cf * (45.0/100.0));
+
+            Console.WriteLine("Valor da Comissão do vendedor => {0:N}", comissao);
+            Console.WriteLine("Valor dos Impostos => {0:N}", impostos);
+            Console.WriteLine("Custo Final do Veículo => {0:N}", custo_final);
+
+            Console.ReadKey();
+
+        }
+    }
+}
